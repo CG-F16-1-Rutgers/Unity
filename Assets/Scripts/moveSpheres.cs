8d3ba0c10e1d2dd0f8fd6f3f9dab175dcb37478d
Zacharias Psarakis <Zacharias Psarakis>
2016-09-26 19:18:26 -0500

    Map, Obstacles, Moving Obstacles, FreeCam, Maze, NavMesh Obstacles

---
+++ Assets/Scripts/moveSpheres.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveSpheres : MonoBehaviour {
+	private float deltaX = 0.05f;
+	private float distance = 0.0f;
+	public int direction;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+
+
+	void Update () {
+		distance += deltaX;
+		rb.AddForce (new Vector3 (direction*25, 0, 0));
+		if (distance >= 10) {
+			distance = 0;
+			direction = -1*direction;
+		}
+
+	
+	}
+}
